3398c2f8a81d2b6cdbb2312415a5e64d36c2ce34
tuyennt <thanhtuyen170993@gmail.com>
2019-04-21 21:07:38 +0700

    Add jsconfig.json for convenient development when resolve module path
    Ex: import X from 'components/Avatar' instead of import X from '../../components/Avatar'

---
+++ src/containers/PostDetail/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Avatar from "components/Avatar";
+
+export default class PostDetail extends React {
+  render() {
+    return (
+      <div>
+        <Avatar />
+      </div>
+    );
+  }
+}
