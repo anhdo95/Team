637e7387b8a6dafef458ceeb625cf7c4000fad7a
minimal-dev <tuyennt.social@gmail.com>
2019-04-22 23:22:05 +0700

    WIP Post detail

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import './index.scss';
 
 export default class Avatar extends Component {
   render() {
-    return <div />;
+    const sizeClass = this.props.size === 'small' ? 'avatar-sm' : 'avatar-md';
+    const classes = `d-flex align-items-center ${sizeClass} justify-content-center`;
+    return (
+      <div className={classes}>
+        <div>{this.props.text}</div>
+      </div>
+    );
   }
 }
--- src/containers/PostDetail/index.js
+++ src/containers/PostDetail/index.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Avatar from 'components/Avatar';
+import './index.scss';
 
 export default class PostDetail extends Component {
   render() {
     return (
       <div>
-        <h1>Post Detail</h1>
+        <header className="d-flex align-items-center mb-3">
+          <Avatar text="S" size="small" />
+          <h1 className="heading">Do you think people should be different from others?</h1>
+          <button className="btn btn-primary ml-auto">Ask question</button>
+        </header>
+
       </div>
     );
   }
