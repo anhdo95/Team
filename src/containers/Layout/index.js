91750dec329feb15a934dd3113e7885615495844
anhdo <dinhanhdo95@gmail.com>
2019-04-27 12:14:11 +0700

    [ADD][POST-LISTING] - build headline and filter options

--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -11,7 +11,7 @@ export default class Layout extends Component {
         <TopBar />
         <main className="main mx-auto">
           <LeftSidebar />
-          <section className="content bg-light">
+          <section className="content">
             <div className="content__inner">{this.props.children}</div>
           </section>
         </main>
--- src/containers/Questions/index.js
+++ src/containers/Questions/index.js
@@ -1,25 +1,95 @@
 import React, { Component } from 'react';
 import channel from 'assets/image/channel.png';
 
+import 'scss/pages/_post-listing.scss';
+
 export default class PostListing extends Component {
-  render() {
+  renderHeadLine() {
     return (
-      <>
-        <div className="content__headline d-flex">
-          <div className="content__headline__avatar">
+      <div className="post-listing__headline d-flex align-items-center justify-content-between">
+        <div className="d-flex">
+          <div className="post-listing__headline__avatar mr-2">
             <img className="w-100 h-100" src={channel} alt="channel" />
           </div>
-          <h1>SASS Viet Nam Questions</h1>
-          <div>
-            <a className="btn btn-primary" href="/">
-              <i className="fa fa-key" aria-hidden="true" />
-              &nbsp;Ask Private Question
-            </a>
+          <h1 className="post-listing__headline__title">SASS Viet Nam Questions</h1>
+        </div>
+        <div className="post-listing__headline__ask-private">
+          <a className="btn btn-primary" href="/">
+            <i className="fa fa-key" aria-hidden="true" />
+            &nbsp;Ask Private Question
+          </a>
+        </div>
+      </div>
+    );
+  }
+
+  renderFilter() {
+    return (
+      <div className="post-listing__filter d-flex align-items-center justify-content-between py-4">
+        <div>100 questions</div>
+        <div>
+          <a href="/" className="post-listing__filter__answered mr-3">
+            Answer questions
+          </a>
+          <div className="btn-group" role="group">
+            <button
+              type="button"
+              className="post-listing__filter__option post-listing__filter__option--active btn btn-outline-secondary"
+              title="The most recently asked questions"
+            >
+              Newest
+            </button>
+            <button type="button" className="post-listing__filter__option btn btn-outline-secondary">
+              Active
+            </button>
+
+            <div className="btn-group" role="group">
+              <button
+                id="btn-filter-more"
+                type="button"
+                className="post-listing__filter__option btn btn-outline-secondary dropdown-toggle"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                More&nbsp;
+              </button>
+              <div className="post-listing__filter__option__more dropdown-menu dropdown-menu-right" aria-labelledby="btn-filter-more">
+                <a className="dropdown-item" href="/">Bounties</a>
+                <a className="dropdown-item" href="/">Votes</a>
+                <a className="dropdown-item" href="/">Frequent</a>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="content__inner__questions bg-warning" />
-        <div className="content__inner__right-sidebar bg-info" />
-      </>
+      </div>
+    );
+  }
+
+  renderPosts() {
+    return (
+      <div className="post-listing__posts bg-warning">
+        <p>You Might Not Need Redux</p>
+      </div>
+    );
+  }
+
+  renderRightSidebar() {
+    return (
+      <div className="post-listing__right-sidebar bg-info" />
+    )
+  }
+
+  render() {
+    return (
+      <div className="post-listing d-flex">
+        <div className="post-listing__quest">
+          {this.renderHeadLine()}
+          {this.renderFilter()}
+          {/* {this.renderPosts()} */}
+        </div>
+        {this.renderRightSidebar()}
+      </div>
     );
   }
 }
